cb8883300974f14cce9465b421c10c14d0bea036
RagingGrim <armandconnor.duplooy@gmail.com>
2016-10-11 12:08:37 +0200

    Added doxygen documentation for AuditoriumMemento.

--- src/AuditoriumMemento.h
+++ src/AuditoriumMemento.h
@@ -23,17 +23,50 @@ public:
       void print(std::ostream & = std::cout) const;
       std::string getId() const;
       //Unique Functions
-	void bind(Auditorium *auditorium); /**< When a memento is constructed it will be bound to an auditorium and the auditorium bound to it.*/
-	void add_command(std::string); /**< Adds a command to the list.*/
-	void undo(); /**< Removes the command at the top of the list and executes it.*/
-	void exec(std::string command);/**< Executes a command.*/
-	void callback(size_t *x , size_t *y , short *state); /**< Sets the callback variables for the memento.*/
-	void Unlock(){ /**< Unlocks the spinlock.*/
+
+	/*! Binds memento to an auditorium.
+	This enables all my callbacks.
+	\param auditorium: An auditorium pointer to bind to.
+	*/
+	void bind(Auditorium *auditorium);
+
+	/*! Adds a command to the memento.
+	\param auditorium: A std::string command to add to the memento.
+	*/
+	void add_command(std::string com);
+
+	/*! Undo's an operation in the auditorium.
+	*/
+	void undo();
+
+	/*! Executes a command.
+	\param command: The string to execute as a command.
+	*/
+	void exec(std::string command);
+
+	/*! Registers callback variables ( for updating the display in the GUI ).
+	\param x: A pointer to size_t representing the x-axis.
+	\param y: A pointer to size_t representing the y-axis.
+	\param state: A pointer to a short representing the state of the seat.
+
+	*/
+	void callback(size_t *x , size_t *y , short *state);
+
+	/*! Unlocks the spinlock.
+	*/
+	void Unlock(){
 		spinLock = false;
 	}
-	void Lock(){ /**< Locks the spinlock.*/
+
+	/*! Locks the spinlock.
+	*/
+	void Lock(){
 		spinLock = true;
 	}
+
+	/*! Determines whether the spinLock is locked or not
+	\return true/false if the spinLock is enabled/disabled.
+	*/
 	bool isLocked(){/**< Tests if the spinlock is locketd.*/
 		return spinLock;
 	}
@@ -41,7 +74,13 @@ public:
 	short *_state;
 	bool guiMode;
 private:
-	 std::string extract(std::string buffer , std::string begin , std::string end);/**< Extracts a substring.*/
+	/*! Scrapes strings from other strings.
+	\param buffer: The buffer to scrape from.
+	\param begin: The string representing the first identifier.
+	\param end:  The string representing the end identifier.
+	\returns The string scraped from buffer.
+	*/
+	 std::string extract(std::string buffer , std::string begin , std::string end);
 	 bool spinLock;
 	Auditorium *auditorium;
 	std::vector<std::string> commands;
