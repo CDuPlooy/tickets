a54e0708d6f1749d550579e3e63a8439ef91744e
RagingGrim <armandconnor.duplooy@gmail.com>
2016-10-11 13:15:35 +0200

    Added doxygen documentation for List and Container.

--- src/Container.h
+++ src/Container.h
@@ -13,15 +13,41 @@ protected:
 public:
       Container();
       ~Container();
-      virtual Object *getCurrent() = 0;/**< Returns the current object pointer or cursor if you will.*/
 
-      virtual void add(Object *object) = 0; /**< Adds an object to the container */
-      virtual void next() = 0; /**< Iterates to the next instance of Object in Container*/
-      virtual void previous() = 0; /**<Iterates to the previous instance of Object in Container*/
-      virtual void push_back(Object *object) = 0; /**< Adds an Object to the list.*/
-      virtual void pop_back() = 0; /**< Removes an Object from the list.*/
-      Object *current; /**<A pointer to an Object instance. */
-      virtual bool isAtEnd() = 0; /**<Returns true when the end of the Container has been reached. */
+	/*! Gets the current object / cursor
+	\return An object pointer or a cursror.
+	*/
+      virtual Object *getCurrent() = 0;
+
+	/*! Gets the current object / cursor
+	\return An object pointer or a cursror.
+	*/
+      virtual void add(Object *object) = 0;
+
+	/*! Sets the cursor to the next object.
+	*/
+      virtual void next() = 0;
+
+	/*! Sets the cursor to the previous  object.
+	*/
+	virtual void previous() = 0;
+
+	/*! Adds an object to the Container.
+	\param object: A pointer to an Object.
+	*/
+	virtual void push_back(Object *object) = 0;
+
+	/*! Removes an object * from the top of the Container.
+	*/
+	virtual void pop_back() = 0;
+
+
+	Object *current; /**<A pointer to an Object instance. */
+
+	/*! Determines whether the cursor is at the end of the Container.
+	\return Returns true when the cursor is at the end of the Container.
+	*/
+	virtual bool isAtEnd() = 0;
       //Inheritance from object
       bool isNull(void) const;
       int compare(Object const &) const;
--- src/List.h
+++ src/List.h
@@ -20,15 +20,50 @@ public:
       void print(std::ostream & = std::cout) const = 0;
       std::string getId() const = 0;
       //Virtual functions
-      virtual Object *at(size_t i) = 0; /**< Returns the object at i.*/
-      virtual void add(Object *object) = 0; /**< Adds an object to the container */
-      virtual void next() = 0; /**< Iterates to the next instance of Object in Container*/
-      virtual void previous() = 0; /**<Iterates to the previous instance of Object in Container*/
-      virtual void push_back(Object *object) = 0; /**< Adds an Object to the list.*/
-      virtual void pop_back() = 0; /**< Removes an Object from the list.*/
+
+	/*! Gets the  object at position i.
+	\param i: The position of the object in the data structure.
+	\return An object pointer.
+	*/
+      virtual Object *at(size_t i) = 0;
+
+	/*! Adds an object to the data structure.
+	\param object: An object pointer.
+	*/
+	virtual void add(Object *object) = 0;
+
+	/*! Moves the cursor to the next object.
+	*/
+      virtual void next() = 0;
+
+	/*! Moves the cursor to the previous object.
+	*/
+      virtual void previous() = 0;
+
+	/*! Adds an object to the List.
+	\param object: A pointer to an Object.
+	*/
+      virtual void push_back(Object *object) = 0;
+
+	/*! Removes an object * from the top of the List.
+	*/
+      virtual void pop_back() = 0;
+
+	/*! Sets the cursor.
+	\param object: The object pointer to set the cursor to.
+	*/
       virtual void setCurrent(Object *object) = 0;
+
+
+	/*! Gets the current object / cursor
+	\return An object pointer or a cursror.
+	*/
       virtual Object *getCurrent() = 0;
-      virtual bool isAtEnd() = 0; /**<Returns true when the end of the Container has been reached. */
+
+	/*! Determines whether the cursor is at the end of the Container.
+	\return Returns true when the cursor is at the end of the Container.
+	*/
+      virtual bool isAtEnd() = 0;
 
       //Unique Functions (To List)
 };
