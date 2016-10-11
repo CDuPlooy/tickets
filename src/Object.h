ce38e8b3f702de4a71944dc52ce4344cb97b59ab
RagingGrim <armandconnor.duplooy@gmail.com>
2016-10-11 14:29:58 +0200

    Added doxygen comments for Object.

--- src/Object.h
+++ src/Object.h
@@ -9,9 +9,26 @@ protected:
 public:
       Object(){};
       virtual ~Object(){};
+
+	/*! Determines whether an object is an instance of NullObject.
+	\return true/false if object is of type of NULL.
+	*/
       virtual bool isNull(void) const = 0 ;
-      virtual int compare(Object const &) const = 0;
-      virtual void print(std::ostream & = std::cout) const = 0;
+
+	/*! Compares two objects.
+	\param obj: The object to compare to the caller.
+	\return true/false if equal/not.
+	*/
+      virtual int compare(Object const &obj) const = 0;
+
+	/*! Outputs the object.
+	\param out: The stream to output to ( default is std::cout  ) .
+	*/
+      virtual void print(std::ostream &out = std::cout) const = 0;
+
+	/*! A string representation of the object type.
+	\return  A std::string representing the object type.
+	*/
       virtual std::string getId() const = 0;
 private:
 };
