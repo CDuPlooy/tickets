e6e55be086af862b6f06940e0c94b4b24cf02224
grim <armandconnor.duplooy@gmail.com>
2016-11-04 10:36:13 +0200

    Changed the main source file , redo the demo! Also this commit might break something with Qt but I don't have the dependencies setup yet so that might be why it's not working. Remove comments in source files in Qt/ directory if it breaks.

--- src/main.cpp
+++ src/main.cpp
@@ -9,200 +9,7 @@ using namespace std;
 bool guiMode;
 
 void testCases(){
-			cout << "Testing ticket printer!" << endl;
-		      FixedAuditorium *faA = new FixedAuditorium(2,2);
-			faA->enablePrinter(true);
-		      faA->setName("Brooklyn");
-		      Minor *personA = new Minor();
-			personA->setName("Flappy");
-		      faA->book(personA,0, 0);
-
-		      cout << "_____________DONE______________" << endl;
-		      delete faA;
-		      delete personA;
-
-		//
-		// cout << "Testing the flexi matrix" << endl;
-		// FlexiAuditorium *flex = new FlexiAuditorium(3,3);
-		// // flex->fillRand();
-		// // flex->print(cout);
-		// // cout << "Can iterate bot : " << flex->iterateBot(0,1) << endl;
-		// // cout << "Can iterate top : " << flex->iterateTop(0,1) << endl;
-		// //
-		// // cout << "Can iterate left : " << flex->iterateLeft(0,1) << endl;
-		// // cout << "Can iterate right : " << flex->iterateRight(0,1) << endl;
-		// //
-		// // cout << "_____________DONE______________" << endl;
-		// delete flex;
-		// return 0;
-
-		cout << "Testing the dynamicAuditorium" << endl;
-		FixedAuditorium *dynAud = new FixedAuditorium(5,5);
-		Minor *Darky = new Minor();
-		dynAud->enableMemento(true);
-		// dynAud->getMemento()->guiMode = false;
-		dynAud->book(Darky, 0, 0);
-		dynAud->print(cout);
-
-		dynAud->undo();
-		dynAud->print(cout);
-		dynAud->print(cout);
-
-
-		delete dynAud;
-		delete Darky;
-		cout << "_____________DONE______________" << endl;
-		cout << RED "tickets - COS121 ;\nGui: " << guiMode << RESET << endl;
-
-		cout << "Testing groups!" << endl;
-		Minor *Billy = new Minor();
-		Billy->setName("Billy");
-		Minor *Timmy = new Minor();
-		Timmy->setName("Timmy");
-		Minor *Sally = new Minor();
-		Sally->setName("Sally");
-		Group *group = new Group();
-		group->push_back(Billy);
-		group->push_back(Timmy);
-		group->push_back(Sally);
-
-
-		FixedAuditorium *Brooklyn = new FixedAuditorium(5,5);
-		Brooklyn->enableMemento(true);
-		Brooklyn->bookAdv(*group);
-		Brooklyn->getMemento()->print(cout);
-			Brooklyn->print(cout);
-		Brooklyn->undo();
-			Brooklyn->print(cout);
-		Brooklyn->getMemento()->print(cout);
-		Brooklyn->undo();
-			Brooklyn->print(cout);
-		Brooklyn->undo();
-			Brooklyn->print(cout);
-
-		Brooklyn->getMemento()->print(cout);
-
-
-		Brooklyn->print(cout);
-
-		cout << "_____________DONE______________" << endl;
-		delete Billy;
-		delete Timmy;
-		delete Sally;
-		delete group;
-		delete Brooklyn;
-
-		cout << "Testing memento!" << endl;
-		FixedAuditorium *memAud = new FixedAuditorium(5,5);
-		memAud->enableMemento(true);
-		Minor *Kathy = new Minor();
-		Kathy->setName("Kathy");
-		memAud->getMemento()->print(cout);
-		memAud->book(Kathy, 0, 0);
-		cout << "Making a booking." << endl;
-		memAud->getMemento()->print(cout);
-		memAud->print(cout);
-		cout << "Undo the booking." << endl;
-		memAud->undo();
-		memAud->undo();
-		memAud->getMemento()->print(cout);
-
-		memAud->print(cout);
-		cout << "_____________DONE______________" << endl;
-		delete memAud;
-
-
-
-
-
-	      cout << "Testing AuditoriumList!" << endl;
-	      AuditoriumList al;
-	      FixedAuditorium brooklyn(5,5);
-	      brooklyn.setName("Brooklyn");
-	      DynamicAuditorium irene(5,5);
-	      irene.setName("Irene");
-
-	      al.push_back(&brooklyn);
-	      al.push_back(&irene);
-	      for(size_t i = 0 ; i < al.getSize() ; i++)
-	            cout << al.at(i)->getName() << endl;
-	      cout << "_____________DONE______________" << endl;
-
-
-	      cout << "Testing iterator ! " << endl;
-	      Store *itStore = new Store(CDLL);
-	      Minor *Bob = new Minor();
-	      Seat *BobSeat = new Seat(Bob);
-	      FixedAuditorium *fixedaud = new FixedAuditorium(5,5);
-	      itStore->push_back(Bob);
-	      itStore->push_back(BobSeat);
-	      itStore->push_back(fixedaud);
-	      Object *object = itStore->getCurrent();
-
-	      while( itStore->isAtEnd() == false ){
-	      cout << object->getId() << endl;
-	            itStore->next();
-	            object = itStore->getCurrent();
-	      }
-	      cout << "_____________DONE______________" << endl;
-	      delete itStore;
-	      delete Bob;
-	      delete BobSeat;
-	      delete fixedaud;
-
-
-	      cout << "Testing the Store!" << endl;
-	      Store *store = new Store(CVECTOR);
-	      Person *a = new Minor();
-	      Seat *b = new Seat(a);
-	      store->push_back(a);
-	      cout << "Minor should expand to" << "==>" << store->getCurrent()->getId() << std::endl;
-	      store->push_back(b);
-	      store->next();
-	      cout  << "Seat should expand to" << "==>" << store->getCurrent()->getId() << std::endl;
-	      store->previous();
-	      store->pop_back();
-	      cout << "Minor should expand to" << "==>" << store->getCurrent()->getId() << std::endl;
-
-	      delete b;
-	      delete store;
-	      delete a;
-	      cout << "_____________DONE______________" << endl;
-
-
-	      cout << "Testing the fixed size matrix!" << endl;
-	      FixedSizeMatrix *fa = new FixedSizeMatrix(5,5);
-	      Minor person;
-	      Seat *seat = new Seat(&person);
-	      seat->bind(&person);
-	      cout << "\tTesting pushback" << endl;
-	      fa->push_back(seat);
-	      fa->pop_back();
-
-	      fa->dump();
-	      delete fa;
-	      cout << "_____________DONE______________" << endl;
-
-
-
-	      cout << "Testing the auditorium modeller!" << endl;
-	      AuditoriumModeller *am = new AuditoriumModeller();
-	      am->loadFromFile("test.txt");
-	      am->getAuditorium()->print(cout);
-	      cout << am->getAuditorium()->dumpRaw() << endl;
-	      am->getAuditorium()->dumpFile("Raw.txt");
-	      cout << "_____________DONE______________" << endl;
-	      delete am;
-
-	      cout << "Testing ListAsVector!" << endl;
-	      ListAsSLL *lv = new ListAsSLL();
-	      lv->add(lv);
-	      lv->add(lv);
-	      lv->add(lv);
-	      lv->previous();
-	      cout << lv->getCurrent()->getId() << endl;
-	      delete lv;
-	      cout << "_____________DONE______________" << endl;
+		cout << "PENDING" << endl;
 
 }
 void pauseC(){
@@ -404,13 +211,15 @@ void testMemento(){
 int main( int argc , char **argv ){
 	// testMemento();
 
-	demo();
 
-	cout << "This concludes the CLI demo of tickets , to run the debug tests press <d> , to exit press <e>" << endl;
+
+	cout << "Welcome to the demo version of tickets! Press <s> for the demo/case examples or press <d> for debugging tests" << endl;
 	char decision;
 	cin >> decision;
 	if(decision == 'd')
 		testCases();
+	if(decision == 's')
+		demo();
       return 0;
 }
 
